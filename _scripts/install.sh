b94551defb907cbedc08a2c9ef0f869554a98673
duanhongyi <duanhongyi@doopai.com>
2022-01-27 22:14:39 +0800

    chore(workflow): add nr_hugepages

--- _scripts/install.sh
+++ _scripts/install.sh
@@ -54,9 +54,14 @@ function configure_os {
   mount bpffs -t bpf /sys/fs/bpf
   rmem_max=$(sysctl -ne net.core.rmem_max)
   if [ ! -n "$rmem_max" ] || [ 2500000 -gt $rmem_max ] ;then
-      echo 'net.core.rmem_max=2500000' >> /etc/sysctl.conf
-      sysctl -p
+      echo 'net.core.rmem_max = 2500000' >> /etc/sysctl.conf
+      
   fi
+  nr_hugepages=$(sysctl -ne vm.nr_hugepages)
+  if [ ! -n "$nr_hugepages" ] || [ 1024 -gt $nr_hugepages ] ;then
+      echo 'vm.nr_hugepages = 1024' >> /etc/sysctl.conf
+  fi
+  sysctl -p
 }
 
 function configure_registries {
