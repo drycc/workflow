a1bb80bccc9060ced510c44f18a5e0c7a09af372
duanhongyi <duanhongyi@doopai.com>
2022-08-24 09:28:41 +0800

    chore(workflow): add replicas settings

--- _scripts/install.sh
+++ _scripts/install.sh
@@ -330,7 +330,8 @@ global:
   ingressClass: traefik
 
 builder:
-  imageRegistry: ${DRYCC_REGISTRY} 
+  replicas: ${BUILDER_REPLICAS}
+  imageRegistry: ${DRYCC_REGISTRY}
   service:
     type: LoadBalancer
     annotations:
@@ -338,6 +339,7 @@ builder:
       metallb.universe.tf/allow-shared-ip: drycc
 
 database:
+  replicas: ${DATABASE_REPLICAS}
   imageRegistry: ${DRYCC_REGISTRY}
   limitsMemory: "256Mi"
   limitsHugepages2Mi: "256Mi"
@@ -352,10 +354,12 @@ fluentd:
     CONTAINER_TAIL_PARSER_TYPE: "/^(?<time>.+) (?<stream>stdout|stderr)( (?<tags>.))? (?<log>.*)$/"
 
 controller:
+  replicas: ${CONTROLLER_REPLICAS}
   imageRegistry: ${DRYCC_REGISTRY} 
   appStorageClass: ${CONTROLLER_APP_STORAGE_CLASS:-"drycc-storage"}
 
 redis:
+  replicas: ${REDIS_REPLICAS}
   imageRegistry: ${DRYCC_REGISTRY}
   persistence:
     enabled: true
@@ -374,17 +378,20 @@ storage:
       storageClass: ${STORAGE_MINIO_PERSISTENCE_STORAGE_CLASS:-""}
   meta:
     pd:
+      replicas: ${STORAGE_META_PD_REPLICAS}
       persistence:
         enabled: true
         size: ${STORAGE_META_PD_PERSISTENCE_SIZE:-10Gi}
         storageClass: ${STORAGE_META_PD_PERSISTENCE_STORAGE_CLASS:-""}
     tikv:
+      replicas: ${STORAGE_META_TIKV_REPLICAS}
       persistence:
         enabled: true
         size: ${STORAGE_META_TIKV_PERSISTENCE_SIZE:-10Gi}
         storageClass: ${STORAGE_META_TIKV_PERSISTENCE_STORAGE_CLASS:-""}
 
 rabbitmq:
+  replicas: ${RABBITMQ_REPLICAS}
   imageRegistry: ${DRYCC_REGISTRY}
   username: "${RABBITMQ_USERNAME}"
   password: "${RABBITMQ_PASSWORD}"
@@ -397,6 +404,7 @@ imagebuilder:
   imageRegistry: ${DRYCC_REGISTRY}
 
 influxdb:
+  replicas: ${INFLUXDB_REPLICAS}
   imageRegistry: ${DRYCC_REGISTRY}
   persistence:
     enabled: true
@@ -404,6 +412,7 @@ influxdb:
     storageClass: ${INFLUXDB_PERSISTENCE_STORAGE_CLASS:-""}
 
 logger:
+  replicas: ${LOGGER_REPLICAS}
   imageRegistry: ${DRYCC_REGISTRY}
 
 monitor:
@@ -418,11 +427,13 @@ monitor:
 
 
 passport:
+  replicas: ${PASSPORT_REPLICAS}
   imageRegistry: ${DRYCC_REGISTRY}
   adminUsername: ${DRYCC_ADMIN_USERNAME}
   adminPassword: ${DRYCC_ADMIN_PASSWORD}
 
 registry:
+  replicas: ${REGISTRY_REPLICAS}
   imageRegistry: ${DRYCC_REGISTRY}
 
 registry-proxy:
